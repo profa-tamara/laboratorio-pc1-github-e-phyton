1a0d9bfa738e44e3cedb4617da6c64d011498ddb
joaovtrrsoares <128906872+joaovtrrsoares@users.noreply.github.com>
2023-04-04 11:54:07 -0300

    LA

--- code_responses.py
+++ code_responses.py
@@ -2,15 +2,47 @@
 
 # Questão 5
 # Sua resposta a partir desta linha
-
+num1 = 5
+num2 = 6
+print("Número 1:", num1)
+print("Número 2:", num2)
+print("O resultado de Número 1 + Número 2 é:", num1 + num2)
 
 # Questão 6
 # Sua resposta a partir desta linha
-
+livro = 60.00
+desconto = (livro * 0.3)
+print("Livro:", livro)
+print("Desconto de 30%:", desconto)
+print("O preço final do livro com desconto é: R$", livro - desconto)
 
 # Questão 7
 # Sua resposta a partir desta linha
-
+F = 194
+formula = "C = ( ( F - 32 ) * 5 ) / 9"
+conversão = ((F - 32)*5)/9
+print("Temperatura em graus ºF:", F)
+print("Formula:", formula)
+print("Temperatura em graus ºC:", conversão)
+print("Portanto, para", (F), "ºF (graus Fahrenheit)", ", temos", (conversão), "ºC (graus Celsius")
 
 # Questão 8
 # Sua resposta a partir desta linha
+print("A) '99' é igual a 99?")
+a = 99 == 99
+
+print("B) '99' 25 é maior do que 39?")
+b = 25 > 39
+
+print("C) 0.9 é menor ou igual a 0.8?")
+c = 0.9 <= 0.8
+
+print("D) 71 é maior ou igual a 60?")
+d = 71 > 60
+
+print("---------------------------------")
+
+print("Resposta de A:", a)
+print("Resposta de B:", b)
+print("Resposta de C:", c)
+print("Resposta de D:", d)
