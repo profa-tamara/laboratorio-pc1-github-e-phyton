4df2fe3f907e98269dd525a8d33a58c2c32ba5ce
Marlhon <128907291+Marlhon@users.noreply.github.com>
2023-04-04 20:04:44 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -1,16 +1,41 @@
-# Escreva as suas respostas neste arquivo.
 
 # Questão 5
-# Sua resposta a partir desta linha
+
+num1=3
+num2=4
+sum=num1+num2
+
+print(sum)
+
 
 
 # Questão 6
-# Sua resposta a partir desta linha
+
+preço_inicial_livro = 50.00;
+preço_final_livro = preço_inicial_livro * 0.7;
+
+print("O preço do livro com desconto é: R$");
+print(preço_final_livro);
+
 
 
 # Questão 7
-# Sua resposta a partir desta linha
+
+t_farenheit = 100;
+t_celsius = ( ( t_farenheit - 32 ) * 5 ) / 9;
+
+print(t_celsius)
+
 
 
 # Questão 8
-# Sua resposta a partir desta linha
+
+a = '99' == 99;
+b = 25 > 39;
+c = 0.9 <= 0.8;
+d = 71 >= 60;
+
+print(a)
+print(b)
+print(c)
+print(d)
