afdbfd174f948e8dbe2166f459ef28327d8859f0
joaoemmanuel-eng <129763964+joaoemmanuel-eng@users.noreply.github.com>
2023-04-04 15:19:49 -0300

    Resposta 5 a 8

--- code_responses.py
+++ code_responses.py
@@ -3,14 +3,32 @@
 # Questão 5
 # Sua resposta a partir desta linha
 
+numero1 = 14
+numero2 = 23
+
+print(numero1 + numero2)
 
 # Questão 6
 # Sua resposta a partir desta linha
 
+valor = 49.90
+desconto = 0.7
+novovalor = preço * desconto
+
+print('O preço do livro com desconto é: R$', novovalor)
 
 # Questão 7
 # Sua resposta a partir desta linha
 
+F = 97
+C = ((F - 32) * 5) / 9
+
+print(F, 'Fahrenheit =', C, 'Celsius')
 
 # Questão 8
 # Sua resposta a partir desta linha
+
+print('99' == 99)
+print(25 > 39)
+print(0.9 <= 0.8)
+print(71 >= 60)
