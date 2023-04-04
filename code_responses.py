253ddec6a328e5c0b23c141e81a5b245c6ab76fc
Gabrielaadm <128913355+Gabrielaadm@users.noreply.github.com>
2023-04-04 07:11:17 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -10,10 +10,15 @@ print (soma);
 
 # Questão 6
 # Sua resposta a partir desta linha
-
+preço = float(input('Qual o preço do livro? R$'))
+novo = preço - (preço * 30 / 100)
+print ('O preço do livro que custava R${} com desconto é: R${}'. format(preço, novo))
 
 # Questão 7
 # Sua resposta a partir desta linha
+f = float(input('Informe a temperatura em °f:'))
+c = ((f - 32  ) * 5) / 9
+print ("A temperatura de {} °F corresponde a {} °C".format (f , c))
 
 
 # Questão 8
