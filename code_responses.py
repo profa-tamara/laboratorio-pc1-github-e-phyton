5cbfb08280d8518f9dae6dbfbe466eaded9986c5
claraalvesc <128913340+claraalvesc@users.noreply.github.com>
2023-04-04 14:08:50 -0300

    Update code_responses.py
    5- 
    Resposta: 170 
    6- 
    RESPOSTA: valor do livro: 100.0
    desconto de 30%: 30.0
    O valor do livro com desconto é: R$ 70.0
    
    7- 
    RESPOSTA: O valor de 95 Fahrenheit em Celsius = 35.0
    
    8- True
    False
    False
    True

--- code_responses.py
+++ code_responses.py
@@ -2,15 +2,31 @@
 
 # Questão 5
 # Sua resposta a partir desta linha
+num1= 120
+num2= 50 
+print (num1 + num2)
 
 
 # Questão 6
 # Sua resposta a partir desta linha
+livro = 100.00
+desconto = (100 * 0.3)
+print ("valor do livro:", livro)
+print (" desconto de 30%:", desconto )
+print ("O valor do livro com desconto é: R$" , livro- desconto )
 
 
 # Questão 7
 # Sua resposta a partir desta linha
+F=95
+C= ( ( F - 32 ) * 5 ) / 9
+print ( C )
+print ( "O valor de 95 Fahrenheit em Celsius =" , C)
 
 
 # Questão 8
 # Sua resposta a partir desta linha
+print ( 99 == 99)
+print (25 > 29)
+print ( 0.9 <= 0.8)
+print (71 >= 60)
