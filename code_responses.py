daf94457ab1a34c62317521a6e241d608cfefdae
GiovanaFerraz <128907496+GiovanaFerraz@users.noreply.github.com>
2023-04-04 17:21:55 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -8,7 +8,9 @@ print (x + y)
 
 # Questão 6
 # Sua resposta a partir desta linha
-
+livro=30
+desc= 0.7
+print("O preço do livro com desconto é: R$" ,livro * desc)
 
 # Questão 7
 # Sua resposta a partir desta linha
