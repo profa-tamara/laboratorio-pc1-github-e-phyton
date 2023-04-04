13d43cd79ddf7382918752d7e172a04bf95f8c4e
Thiago Elias <128907151+thiagoelias04@users.noreply.github.com>
2023-04-04 11:28:14 -0300

    respostas da parte 2 Thiago Elias

--- code_responses.py
+++ code_responses.py
@@ -3,14 +3,23 @@
 # Questão 5
 # Sua resposta a partir desta linha
 
+a= 10;
+b= 20;
+print (a + b)
 
 # Questão 6
 # Sua resposta a partir desta linha
 
+valor = float(input('Digite o valor do livro R$'))
+desconto = valor - (valor * 30 / 100)
+print ('O preço do livro de R${} com desconto é: R${}'. format(valor, desconto))
 
 # Questão 7
 # Sua resposta a partir desta linha
 
+far = float(input('Informe °f:'))
+cel = ((far - 32  ) * 5) / 9
+print ("A temperatura de {} °F equivale a {} °C".format (far , cel))
 
 # Questão 8
 # Sua resposta a partir desta linha
