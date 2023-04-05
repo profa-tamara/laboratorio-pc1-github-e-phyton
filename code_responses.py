018aa24205619006373bf039fdf2139a858af5cd
Guilherme Augusto <128913243+GuilhermeAugusto-ai@users.noreply.github.com>
2023-04-05 18:42:49 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -1,16 +1,52 @@
-# Escreva as suas respostas neste arquivo.
+# questão 5 
 
-# Questão 5
-# Sua resposta a partir desta linha
+a = 10
+b = 15
+
+print(a + b)
+
+#questão 6
+
+livro = 100
+
+print("o preco do livro com desconto e :", livro * ((100/100) - (30/100)))
+
+#questão 7 
+
+temp_fah = 100
+
+temp_celcius = ( (temp_fah - 32) * 5) / 9
+
+print (temp_celcius)
+
+#questão 8 
+
+a = 99; b = '99'
+c = 25; d = 39
+e = 0.9; f = 0.8
+g = 71; h = 60
+
+if a == b:
+   print("99 e igual a 99?:" , 'Verdade')
+
+else:
+   print("99 e igual a 99?", 'Falso')
+
+if c > d:
+   print("25 e maior que 39?:", 'Verdade')
+
+else:
+   print("25 e maior que 39?:", 'Falso')
+
+if e <= f:
+   print("0.9 e menor ou igual a 0.8?:", 'Verdade')
+
+else:
+   print("0.9 e menor ou igual a 0.8?:", 'Falso')
 
 
-# Questão 6
-# Sua resposta a partir desta linha
+if g >= h:
+   print("71 e maior ou igual a 60?:", 'Verdade')
 
-
-# Questão 7
-# Sua resposta a partir desta linha
-
-
-# Questão 8
-# Sua resposta a partir desta linha
+else:
+   print('71 e maior ou igual a 60?:", Falso')
