63c568c61ccb0b3181fe79b175fed07819830981
NicoleKAraujo <128907702+NicoleKAraujo@users.noreply.github.com>
2023-04-05 11:10:33 -0300

    Resposta parte 2 - NicoleKAraujo

--- code_responses.py
+++ code_responses.py
@@ -2,15 +2,42 @@
 
 # Questão 5
 # Sua resposta a partir desta linha
+num1 = 2022
+num2 = 2023
+soma = (num1 + num2)
+print (soma)
 
 
 # Questão 6
 # Sua resposta a partir desta linha
+livro = 40
+desconto = (40 * 0.3)
+print ("O valor do livro é: R$ ", livro)
+print (" o desconto de 30% é: R$" , desconto)
+print ("O valor do livro com desconto é: R$" , livro - desconto)
+
 
 
 # Questão 7
 # Sua resposta a partir desta linha
+temperaturaf = 35
+temperaturac = ( (temperaturaf - 32) * 5 ) / 9
+print ("A temperatura em graus Celsius é: ", temperaturac)
+
 
 
 # Questão 8
 # Sua resposta a partir desta linha
+a = 99
+b = 25
+c = 39
+d = 0.9
+e = 0.8
+f = 71
+g = 60
+
+a == a
+b > c
+d <= e
+f >= g
+
