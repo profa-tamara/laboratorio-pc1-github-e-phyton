e7a782d819abb080a5bcabe42e7f91702231d8fd
guihenriquecc <128913405+guihenriquecc@users.noreply.github.com>
2023-04-05 03:22:38 -0300

    s

--- code_responses.py
+++ code_responses.py
@@ -7,12 +7,34 @@ print(num1 + num2)
 
 
 # Questão 6
-# Sua resposta a partir desta linha
+precodolivro = 600
+desconto = 0.3
+print('O preço do livro com desconto é:')
+print((precodolivro) - desconto * precodolivro)
 
 
 # Questão 7
-# Sua resposta a partir desta linha
+F = 100
+C = (F - 32) * (5 / 9)
+print("O valor de 100 graus Fahrenheit convertido para gruas Celsius é igual a:")
+print(C)
 
 
 # Questão 8
-# Sua resposta a partir desta linha
+if 99 == 99:
+    print("99 é igual a 99 = true")
+elif 99 != 99:
+    print('99 é igual a 99 = false')
+if 25 > 39:
+    print("25 é maior que 39 = true")
+elif 25 != 39:
+    print('25 é maior que 39 = false')
+if 0.9 <= 0.8:
+     print("0.9 é menor ou igual a 0.8 = true")
+elif 0.9 > 0.8:
+    print('0.9 é menor ou igual a 0.8 = false')
+if 71 >= 60:   
+    print("71 é maior ou igual a 60 = true")
+elif 71 < 60:
+    print('71 é maior ou igual a 60 = false')
+    
