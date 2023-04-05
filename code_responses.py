9b4ad5178a26b14059a6d06ef10dc5cf88d76236
luizarthurr <129861880+luizarthurr@users.noreply.github.com>
2023-04-05 10:44:49 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -1,16 +1,42 @@
 # Escreva as suas respostas neste arquivo.
 
 # Questão 5
-# Sua resposta a partir desta linha
+n1 = 3
+n2 = 5
+soma = n1 + n2
+print(soma)
 
 
 # Questão 6
-# Sua resposta a partir desta linha
+valor_livro = 45.5
+desc = 30/100
+preco_final = valor_livro - (valor_livro * desc)
+print("O preço do livro com desconto é: R$", preco_final)
 
 
 # Questão 7
-# Sua resposta a partir desta linha
+temp_far = 100
+temp_cel = ((temp_far - 32) * 5) / 9
+print(temp_far, "Fahrenheit = ", "%.2f" % temp_cel, "Celsius")
 
 
 # Questão 8
-# Sua resposta a partir desta linha
+#comparacao1 
+
+result_1 = ('99' == 99)
+print(result_1)
+
+#comparacao2
+
+result_2 = (25 > 39)
+print(result_2) 
+
+#comparacao3
+
+result_3 = (0.9 == 0.8)
+print(result_3)
+
+#comparacao4 
+
+result_4 = (71 >= 60)
+print(result_4) 
