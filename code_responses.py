0eecca5d7c3631d3926d0b53d542f49174504fa3
AugustusDayrell <128907204+AugustusDayrell@users.noreply.github.com>
2023-04-04 23:36:09 -0300

    CodeResponse completo

--- code_responses.py
+++ code_responses.py
@@ -1,16 +1,43 @@
 # Escreva as suas respostas neste arquivo.
 
-# Questão 5
-# Sua resposta a partir desta linha
+# Questão 5 
+
+valor1 = 50
+valor2 = 120
+resultado = (valor1 + valor2)
+print (resultado)
 
 
-# Questão 6
-# Sua resposta a partir desta linha
+# Questão 6 
+
+precolivro = 20
+desconto = (precolivro * 0.3)
+novo_valor = (precolivro - desconto)
+print("o preço do livro com desconto é:", novo_valor)
 
 
 # Questão 7
-# Sua resposta a partir desta linha
+
+tempf = 80
+tempc = (((tempf - 32) * 5) / 9 ) 
+print("a temperatura em celsius é:", tempc)
+
 
 
 # Questão 8
-# Sua resposta a partir desta linha
+
+a = 99
+b = (a == a)
+print("99 é igual a 99?", b)
+d = 25
+e = 39
+f = (d > e)
+print("25 é maior que 39?", f)
+g = 0.9
+h = 0.8
+i = (g <= h)
+print("0,9 é menor ou igual a 0,8?", i)
+j = 71
+k = 60
+l = ( j >= k)
+print("71 é maior ou igual a 60?", l)
