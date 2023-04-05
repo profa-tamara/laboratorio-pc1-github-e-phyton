388169fb8faf59f80ff4b22ec84c30034670b128
arthurssrafael <128913307+arthurssrafael@users.noreply.github.com>
2023-04-05 15:45:45 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -2,15 +2,37 @@
 
 # Questão 5
 # Sua resposta a partir desta linha
-
+a = 10
+b = 15
+print(a + b)
 
 # Questão 6
 # Sua resposta a partir desta linha
-
+livro = 30
+desconto = (livro * 0.3)
+novo_valor = (livro - desconto)
+print("o preço do livro com desconto é:", novo_valor)
 
 # Questão 7
 # Sua resposta a partir desta linha
-
+tempf = 90
+tempc = (((tempf - 32) * 5) / 9 ) 
+print("a temperatura em celsius é:", tempc)
 
 # Questão 8
 # Sua resposta a partir desta linha
+a = 99
+b = (a == a)
+print("99 é igual a 99?", b)
+d = 25
+e = 39
+f = (d > e)
+print("25 é maior que 39?", f)
+g = 0.9
+h = 0.8
+i = (g <= h)
+print("0,9 é menor ou igual a 0,8?", i)
+j = 71
+k = 60
+l = ( j >= k)
+print("71 é maior ou igual a 60?", l)
