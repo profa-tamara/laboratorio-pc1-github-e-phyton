d6ddca596fae8cb9351f18374ea49384e2f722f6
joaovtrrsoares <128906872+joaovtrrsoares@users.noreply.github.com>
2023-04-05 08:31:38 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -2,6 +2,7 @@
 
 # Questão 5
 # Sua resposta a partir desta linha
+
 num1 = 5
 num2 = 6
 print("Número 1:", num1)
@@ -10,6 +11,7 @@ print("O resultado de Número 1 + Número 2 é:", num1 + num2)
 
 # Questão 6
 # Sua resposta a partir desta linha
+
 livro = 60.00 #Valor do livro
 d = 30 #Valor do desconto
 desconto = (livro * d/100) #d/100, equivale a x% (nesse exemplo equivale a 30%)
@@ -19,6 +21,7 @@ print("O preço final do livro com desconto é: R$", livro - desconto)
 
 # Questão 7
 # Sua resposta a partir desta linha
+
 F = 194
 formula = "C = ( ( F - 32 ) * 5 ) / 9"
 conversão = ((F - 32)*5)/9
@@ -29,21 +32,26 @@ print("Portanto, para", (F), "ºF (graus Fahrenheit)", ", temos", (conversão), 
 
 # Questão 8
 # Sua resposta a partir desta linha
-print("A) '99' é igual a 99?")
-a = 99 == 99
 
-print("B) '99' 25 é maior do que 39?")
-b = 25 > 39
+print("A) '99' é igual a 99?")
+a = 99
+a1 = a == 99
+
+print("B) 25 é maior do que 39?")
+b = 25
+b1 = b > 39
 
 print("C) 0.9 é menor ou igual a 0.8?")
-c = 0.9 <= 0.8
+c = 0.9
+c1 = c <= 0.8
 
 print("D) 71 é maior ou igual a 60?")
-d = 71 >= 60
+d = 71
+d1 = d >= 60
 
 print("---------------------------------")
 
-print("Resposta de A:", a)
-print("Resposta de B:", b)
-print("Resposta de C:", c)
-print("Resposta de D:", d)
+print("Resposta de A:", a1)
+print("Resposta de B:", b1)
+print("Resposta de C:", c1)
+print("Resposta de D:", d1)
