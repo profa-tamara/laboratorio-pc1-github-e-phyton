d536bd8f7140daddde4cbf45024fcaf0c77b1c17
Guilherme Augusto <128913243+GuilhermeAugusto-ai@users.noreply.github.com>
2023-04-05 23:35:28 -0300

    respostas de 5 a 8

--- code_responses.py
+++ code_responses.py
@@ -21,32 +21,7 @@ print (temp_celcius)
 
 #questão 8 
 
-a = 99; b = '99'
-c = 25; d = 39
-e = 0.9; f = 0.8
-g = 71; h = 60
-
-if a == b:
-   print("99 e igual a 99?:" , 'Verdade')
-
-else:
-   print("99 e igual a 99?", 'Falso')
-
-if c > d:
-   print("25 e maior que 39?:", 'Verdade')
-
-else:
-   print("25 e maior que 39?:", 'Falso')
-
-if e <= f:
-   print("0.9 e menor ou igual a 0.8?:", 'Verdade')
-
-else:
-   print("0.9 e menor ou igual a 0.8?:", 'Falso')
-
-
-if g >= h:
-   print("71 e maior ou igual a 60?:", 'Verdade')
-
-else:
-   print('71 e maior ou igual a 60?:", Falso')
+print('99' == 99)
+print(25 > 39)
+print(0.9 <= 0.8)
+print(71 >= 60)
