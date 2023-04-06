b4e4096fb02f4360e4369057e810bbb7f461a06a
JoaoVitorPuff <128913309+JoaoVitorPuff@users.noreply.github.com>
2023-04-05 23:52:16 -0300

    Update code_responses.py

--- code_responses.py
+++ code_responses.py
@@ -1,16 +1,25 @@
 # Escreva as suas respostas neste arquivo.
-
 # Questão 5
 # Sua resposta a partir desta linha
-
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+print (n1+n2)
 
 # Questão 6
 # Sua resposta a partir desta linha
-
+L = int(input("Qual o valor do livro?"))
+L2 = str(L*0.7)
+print ("O valor do livro com desconto é R$ " + L2)
 
 # Questão 7
 # Sua resposta a partir desta linha
-
+F = float(input("Qual a temperatura em Fahrenheit? "))
+C = str(( ( F - 32 ) * 5 ) / 9)
+print ("Essa temperatura em Celsius é igual a " + C)
 
 # Questão 8
 # Sua resposta a partir desta linha
+print(99 == 99)
+print(25 > 39)
+print(0.9 < 0.8 or 0.9 == 0.8)
+print(71 > 60 or 71 == 60)
